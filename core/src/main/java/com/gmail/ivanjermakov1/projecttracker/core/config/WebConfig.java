0340f5e69826c063eb434bda6e1fee5b06a38885
ivanjermakov <vania6600@gmail.com>
2019-03-11 23:46:15 +0300

    api: cors policy config

---
+++ core/src/main/java/com/gmail/ivanjermakov1/projecttracker/core/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.gmail.ivanjermakov1.projecttracker.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configure correct CORS policy for same host and different port REST API access
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*");
+	}
+	
+}
