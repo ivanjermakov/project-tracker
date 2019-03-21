c0b814ea1f010ca67d2290d067c4e94a0198a184
ivanjermakov <vania6600@gmail.com>
2019-03-21 13:01:10 +0300

    api: fix date representation

--- core/src/main/java/com/gmail/ivanjermakov1/projecttracker/core/dto/UserDto.java
+++ core/src/main/java/com/gmail/ivanjermakov1/projecttracker/core/dto/UserDto.java
@@ -1,10 +1,13 @@
 package com.gmail.ivanjermakov1.projecttracker.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 public class UserDto {
 	
 	public Long id;
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	public LocalDate joined;
 	public UserInfoDto userInfo;
 	public UserCredentialsDto userCredentials;
