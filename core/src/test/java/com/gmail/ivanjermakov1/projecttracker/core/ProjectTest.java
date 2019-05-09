f3fc4c4477bdd9088b73dca51c6d1068f4e15448
ivanjermakov <vania6600@gmail.com>
2019-05-09 14:23:53 +0300

    api: tests

--- core/src/test/java/com/gmail/ivanjermakov1/projecttracker/core/ProjectTest.java
+++ core/src/test/java/com/gmail/ivanjermakov1/projecttracker/core/ProjectTest.java
@@ -4,10 +4,12 @@ import com.gmail.ivanjermakov1.projecttracker.core.controller.AuthController;
 import com.gmail.ivanjermakov1.projecttracker.core.controller.ProjectController;
 import com.gmail.ivanjermakov1.projecttracker.core.controller.RegisterController;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.AuthUserDto;
+import com.gmail.ivanjermakov1.projecttracker.core.dto.EditProjectDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.NewProjectDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.ProjectDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.RegisterUserDto;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.AuthenticationException;
+import com.gmail.ivanjermakov1.projecttracker.core.exception.AuthorizationException;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.NoSuchEntityException;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.RegistrationException;
 import org.junit.Assert;
@@ -17,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,6 +40,7 @@ public class ProjectTest {
 	private ProjectController projectController;
 	
 	private String token;
+	private ProjectDto project;
 	private int projectsCount;
 	
 	@Before
@@ -66,6 +68,64 @@ public class ProjectTest {
 		
 		Assert.assertEquals(2, twoProjects.size());
 		Assert.assertEquals(projectsCount, allProjects.size());
+	}
+	
+	@Test
+	public void shouldGetProject() throws NoSuchEntityException, AuthorizationException {
+		project = projectController.get(
+				token,
+				authController.validate(token).login,
+				projectController.all(token, PageRequest.of(0, 1))
+						.stream()
+						.findFirst().orElseThrow(NoSuchEntityException::new).name
+		);
+		
+		Assert.assertNotNull(project);
+	}
+	
+	@Test
+	public void shouldEditProject() throws AuthorizationException, NoSuchEntityException {
+		ProjectDto project = projectController.all(token, PageRequest.of(0, 1))
+				.stream()
+				.findFirst()
+				.orElseThrow(NoSuchEntityException::new);
+		
+		Assert.assertNotNull(project);
+		
+		project = projectController.edit(token, new EditProjectDto(
+				project.id,
+				project.isPublic,
+				project.name,
+				project.description,
+				project.about
+		));
+		
+		Assert.assertNotNull(project);
+		
+		project = projectController.edit(token, new EditProjectDto(
+				project.id,
+				false,
+				"test_proj_new_name",
+				project.description,
+				project.about
+		));
+		
+		Assert.assertNotNull(project);
+		Assert.assertEquals("test_proj_new_name", project.name);
+	}
+	
+	@Test
+	public void shouldDeleteProject() throws NoSuchEntityException, AuthorizationException {
+		List<ProjectDto> allProjects = projectController.all(token, PageRequest.of(0, Integer.MAX_VALUE));
+		
+		projectController.delete(token, allProjects
+				.stream()
+				.findFirst()
+				.orElseThrow(NoSuchEntityException::new).id);
+
+		List<ProjectDto> allProjectsExceptDeleted = projectController.all(token, PageRequest.of(0, Integer.MAX_VALUE));
+		
+		Assert.assertEquals(allProjects.size() - 1, allProjectsExceptDeleted.size());
 	}
 	
 }
