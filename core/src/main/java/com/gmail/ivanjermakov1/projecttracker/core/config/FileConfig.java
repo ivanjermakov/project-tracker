f0a0a99150d8e1583535e64f302dfdf227ce3af8
ivanjermakov <vania6600@gmail.com>
2019-05-16 21:47:32 +0300

    api: minor

--- core/src/main/java/com/gmail/ivanjermakov1/projecttracker/core/config/FileConfig.java
+++ core/src/main/java/com/gmail/ivanjermakov1/projecttracker/core/config/FileConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FileUploadConfig implements WebMvcConfigurer {
+public class FileConfig implements WebMvcConfigurer {
 	
 	@Value("${fileupload.path}")
 	private String fileuploadPath;
