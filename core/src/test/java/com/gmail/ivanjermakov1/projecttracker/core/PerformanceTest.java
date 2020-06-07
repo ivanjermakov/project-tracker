992bbe17cf57a012e4605d3ff1d44b42022da0c9
ivanjermakov <ivanjermakov1@gmail.com>
2020-06-07 14:58:01 +0300

    better performance testing

--- core/src/test/java/com/gmail/ivanjermakov1/projecttracker/core/PerformanceTest.java
+++ core/src/test/java/com/gmail/ivanjermakov1/projecttracker/core/PerformanceTest.java
@@ -1,16 +1,19 @@
 package com.gmail.ivanjermakov1.projecttracker.core;
 
+import com.gmail.ivanjermakov1.projecttracker.core.controller.ActivityController;
 import com.gmail.ivanjermakov1.projecttracker.core.controller.AuthController;
 import com.gmail.ivanjermakov1.projecttracker.core.controller.ProjectController;
 import com.gmail.ivanjermakov1.projecttracker.core.controller.RegisterController;
+import com.gmail.ivanjermakov1.projecttracker.core.controller.TaskController;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.AuthUserDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.NewProjectDto;
+import com.gmail.ivanjermakov1.projecttracker.core.dto.NewTaskDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.ProjectDto;
 import com.gmail.ivanjermakov1.projecttracker.core.dto.RegisterUserDto;
+import com.gmail.ivanjermakov1.projecttracker.core.entity.enums.TaskType;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.AuthenticationException;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.NoSuchEntityException;
 import com.gmail.ivanjermakov1.projecttracker.core.exception.RegistrationException;
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +23,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -35,38 +37,56 @@ public class PerformanceTest {
 	private AuthController authController;
 	@Autowired
 	private ProjectController projectController;
+	@Autowired
+	private ActivityController activityController;
+	@Autowired
+	private TaskController taskController;
 
-	@Test
-	@Ignore
-	public void users() {
-		IntStream.range(0, 1000).forEach(i -> {
+	//	@Before
+	public void fillDb() {
+		IntStream.range(0, 10000).forEach(i -> {
 			try {
-				registerController.register(new RegisterUserDto(UUID.randomUUID().toString(), "password"));
-			} catch (RegistrationException e) {
+				RegisterUserDto registerUserDto = new RegisterUserDto(UUID.randomUUID().toString(), "password");
+				registerController.register(registerUserDto);
+				String token = authController.authenticate(new AuthUserDto(registerUserDto.login, registerUserDto.password));
+				ProjectDto projectDto = projectController.create(UUID.randomUUID().toString(), new NewProjectDto(true, "test_proj" + i, null, null));
+				taskController.create(token, new NewTaskDto(null, projectDto.id, TaskType.FEATURE, null, null, UUID.randomUUID().toString(), null));
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
 	}
 
 	@Test
-	@Ignore
-	public void projects() throws RegistrationException, AuthenticationException, NoSuchEntityException {
+//	@Ignore
+	public void activity() throws RegistrationException, AuthenticationException, NoSuchEntityException {
 		RegisterUserDto registerUserDto = new RegisterUserDto("test", "password");
 		registerController.register(registerUserDto);
-
 		String token = authController.authenticate(new AuthUserDto(registerUserDto.login, registerUserDto.password));
-		IntStream
-				.range(0, 1000)
-				.forEach(i -> {
-					try {
-						projectController.create(token, new NewProjectDto(true, "test_proj" + i, null, null));
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				});
 
-		List<ProjectDto> projects = projectController.all(token, Pageable.unpaged());
-		Assert.assertEquals(1000, projects.size());
+		IntStream.range(0, 1000).forEach(i -> {
+			try {
+				activityController.allByUser(token, Pageable.unpaged());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	@Test
+//	@Ignore
+	public void searchTask() throws RegistrationException, NoSuchEntityException, AuthenticationException {
+		RegisterUserDto registerUserDto = new RegisterUserDto("test", "password");
+		registerController.register(registerUserDto);
+		String token = authController.authenticate(new AuthUserDto(registerUserDto.login, registerUserDto.password));
+
+		IntStream.range(0, 1000).forEach(i -> {
+			try {
+				taskController.owned(token, Pageable.unpaged());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 
