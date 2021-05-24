448291a75ff350a8e7f98a3ea31f5a7fa2b0776c
ivanjermakov <ivanjermakov1@gmail.com>
2021-05-24 18:40:24 +0300

    User skills; log out

--- web/project-tracker/src/app/component/embedded/header/header.component.ts
+++ web/project-tracker/src/app/component/embedded/header/header.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {LOGO_BW_SRC, LOGO_SRC} from '../../../../globals';
+import {LOCALSTORAGE_TOKEN_NAME, LOGO_BW_SRC, LOGO_SRC} from '../../../../globals';
 import {AppComponent} from '../../../app.component';
 import {TokenProvider} from '../../../provider/token.provider';
 import {AuthService} from '../../../service/auth.service';
 import {User} from '../../../dto/User';
+import {Router} from '@angular/router';
 
 @Component({
 	selector: 'app-header',
@@ -21,7 +22,8 @@ export class HeaderComponent implements OnInit {
 	constructor(
 		private app: AppComponent,
 		private tokenProvider: TokenProvider,
-		private authService: AuthService
+		private authService: AuthService,
+		private router: Router
 	) {
 	}
 
@@ -36,4 +38,12 @@ export class HeaderComponent implements OnInit {
 		});
 	}
 
+	logOut() {
+		if (!confirm('Are you sure want to log out?')) {
+			return;
+		}
+		this.tokenProvider.setToken(null);
+		localStorage.removeItem(LOCALSTORAGE_TOKEN_NAME);
+		this.router.navigate(['/login']);
+	}
 }
